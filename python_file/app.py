c5f52d46f52fdd727333dd28f65c9d457ffc4f58
H. Jaeh <huhj98@naver.com>
2024-05-02 17:49:56 +0900

    Co-authored-by: HOUNSIM <160982674+HOUNSIM@users.noreply.github.com> Co-authored-by: Parkshihwan <160465203+Parkshihwan@users.noreply.github.com>

--- python_file/app.py
+++ python_file/app.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from flask import Flask,request,jsonify
+from keras.models import Sequential
+from keras.layers import Dense
 
-app = FastAPI()
 
-@app.get("/")
-def index():
-    return FileResponse('index.html')
+app = Flask(__name__)
+
+@app.reoute("/",methods=['POST','GET'])
+
+def denseFunc():
+    units=request.form.get('units_val',type=int)
+    activation=request.form.get('act_val',type=str)
+    x_val=request.form.get('x_val',type=list)
+    y_val=request.form.get('y_val',type=list)
+
+    for i in range(1,len(x_val)):
+        x_train=[]
+        i+=1
+        return x_train.append(x_val[i])
+
+    for j in range(1,len(y_val)):
+        y_train=[]
+        j+=1
+        return y_train.append(y_val[j])
+
+    model=Sequential[
+        Dense(units=units,input_dim=x_val.shape[1],activation=activation)
+    ]
+    
+    model.compile(loss='mse',metrics=['accuracy'])
+    model.fit(x_train,y_train,epochs=10)
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
